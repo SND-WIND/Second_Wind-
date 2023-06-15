95444b498b7df21e8502d19cb71b6b00f157bef4
ThyDrSlen <drslen9@gmail.com>
2023-06-14 22:14:04 -0400

    fixed npm run kickstart

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.PG_HOST || '127.0.0.1',
+      host: process.env.PG_HOST || 'localhost',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASS || 'postgres',
-      database: process.env.PG_DB || 'second-wind',
+      password: process.env.PG_PASS || 'pass10',
+      database: process.env.PG_DB || 'postgres',
     },
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
