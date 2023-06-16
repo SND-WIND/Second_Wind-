f76a47e54c5f7cbd14ed1b7ef503ad4157b80b7b
jahshiek <jahshiekwade58@gmail.com>
2023-06-16 12:27:57 -0400

    did login form and css need to add label and fix flexbox

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
 import { logUserIn } from "../adapters/auth-adapter";
 import CurrentUserContext from "../contexts/current-user-context";
+import "../styles/Login.css"
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -21,16 +22,55 @@ export default function LoginPage() {
   if (currentUser) return <Navigate to="/" />;
 
   return <>
-    <h1>Login</h1>
+    <div className='login-Form1'>
+      <div id="logo-black">
+      <img
+        src={logo}
+        alt=""
+      />
+    </div>
+      <h1 className="loginH2">Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            id="login-username"
+            autoComplete="username"  
+            name="username"
+            placeholder="Username"
+            size="35"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            autoComplete="current-password" 
+            name="password"
+            id="login-password"
+            placeholder="Password"
+            size="35"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit" id="login-submit" size="35">Submit</button>
+        <div className="login-createAccount">Dont have an account?<a className="login-createlink"href="">Create account</a></div>
+      </form>
+      { !!errorText && <p>{errorText}</p> } 
+    </div>
+  </>;
+}
+
+{/* <h1>Login</h1>
     <form onSubmit={handleSubmit}>
       <label htmlFor="username">Username</label>
-      <input type="text" autoComplete="username" id="username" name="username" />
+      <input type="text" autoComplete="username"  name="username" />
 
       <label htmlFor="password">Password</label>
       <input type="password" autoComplete="current-password" id="password" name="password" />
 
       <button>Log in!</button>
     </form>
-    { !!errorText && <p>{errorText}</p> }
-  </>;
-}
+    { !!errorText && <p>{errorText}</p> } */}
