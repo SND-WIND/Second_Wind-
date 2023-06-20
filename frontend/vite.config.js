a3303ffbfe5eccfb8c22b52171c047b31c5b99ee
jahshiek <jahshiekwade58@gmail.com>
2023-06-20 10:59:08 -0400

    modified my own config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080/",
+        target: "127.0.0.1:3000",
         changeOrigin: true,
         secure: false,
         ws: true,
