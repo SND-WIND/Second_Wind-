9bd26b65a828de1ca6503c39756c7ffdcce681d8
BayzU <bayzedullah@gmail.com>
2023-06-21 22:51:20 -0400

    Changed Profile Page

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -2,11 +2,13 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate, Navigate, useParams } from "react-router-dom";
 import Menu from "../components/Menu";
 import Feed from "../components/Feed";
+import Messages from "../components/Messages";
 import PostList from "../components/PostList";
 import CurrentUserContext from "../contexts/current-user-context";
 import { getUser } from "../adapters/user-adapter";
 import { logUserOut } from "../adapters/auth-adapter";
 import UpdateUsernameForm from "../components/UpdateUsernameForm";
+import "../styles/User.css"
 
 export default function UserPage() {
   const navigate = useNavigate();
@@ -44,21 +46,22 @@ export default function UserPage() {
     : userProfile.username;
 
   return (
-    <>
+    <div className="profile-container">
       <Menu />
       <PostList />
-      <h1>{profileUsername}</h1>
-      {!!isCurrentUserProfile && (
+      {/* <h1>{profileUsername}</h1> */}
+      {/* {!!isCurrentUserProfile && (
         <button onClick={handleLogout}>Log Out</button>
-      )}
-      <p>If the user had any data, here it would be</p>
-      <p>Fake Bio or something</p>
-      {!!isCurrentUserProfile && (
+      )} */}
+      {/* <p>If the user had any data, here it would be</p>
+      <p>Fake Bio or something</p> */}
+      {/* {!!isCurrentUserProfile && (
         <UpdateUsernameForm
           currentUser={currentUser}
           setCurrentUser={setCurrentUser}
         />
-      )}
-    </>
+      )} */}
+      <Messages />
+    </div >
   );
 }
