7e79c9b1a96715d38cbb053a8a5a400da3b45aa8
ThyDrSlen <drslen9@gmail.com>
2023-06-23 18:21:50 -0400

    seeds for user and posts

--- src/db/seeds/comments_seeds.js
+++ src/db/seeds/comments_seeds.js
@@ -4,11 +4,6 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('comments').del()
-  await knex('comments').insert([
-    {user_id: 8, post_id: 5, comment: 'I agree'},
-    {user_id: 6, post_id: 7, comment: 'As you should'},
-    {user_id: 5, post_id: 2, comment: 'I disagree'}
-  ]);
+ 
 };
 
