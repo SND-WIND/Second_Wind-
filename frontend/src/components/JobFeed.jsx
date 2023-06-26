904c531cde4fe19767128ee1aae3d38e45de37ae
BayzU <bayzedullah@gmail.com>
2023-06-26 15:31:51 -0400

    mergin with main

--- frontend/src/components/JobFeed.jsx
+++ frontend/src/components/JobFeed.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useContext } from "react";
 import JobList from "../components/JobList";
 import JobSearchBar from "./JobSearchBar";
-import CreateJobModal from "./CreateJobModal";
 
 export default function JobFeed() {
   return (
   <div className="job-feed">
     <JobSearchBar />
-    <CreateJobModal />
+    {/* <CreateJobModal /> */}
     <JobList />
   </div>
   );
