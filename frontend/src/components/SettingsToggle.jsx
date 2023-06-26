2557576dc5b16c4cc722b67b06a3deeb9876ceb8
ThyDrSlen <drslen9@gmail.com>
2023-06-25 20:48:11 -0400

    added material ui and buttons to the settings page

--- frontend/src/components/SettingsToggle.jsx
+++ frontend/src/components/SettingsToggle.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import SettingsUserForm from "./SettingsUserFrom";
 import SettingsSecurityForm from "../components/SettingsSecurityForm";
 import SettingsTerminate from "./SettingsTerminate";
+import { Button } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import ShieldIcon from '@mui/icons-material/Shield';
+import ContactEmergencyIcon from '@mui/icons-material/ContactEmergency';
 
 const SettingsToggle = () => {
   const [activeForm, setActiveForm] = useState("user");
@@ -16,21 +20,27 @@ const SettingsToggle = () => {
   return (
     <div className="settings-toggle-form">
       <div className="settings-menu">
+        <Button variant="contained" endIcon={<ContactEmergencyIcon/>}color="primary" onClick={() => toggleForm("user")}>
         <h3 className="settings-menu-item" onClick={() => toggleForm("user")}>
           User
         </h3>
+        </Button>
+        <Button variant="contained" endIcon={<ShieldIcon/>}color="primary" onClick={() => toggleForm("security")}>
         <h3
           className="settings-menu-item"
-          onClick={() => toggleForm("security")}
+  
         >
           Security
         </h3>
+        </Button>
+        <Button variant="contained"  endIcon={<DeleteForeverIcon size="large"/>}color="primary" onClick={() => toggleForm("terminate")}>
         <h3
           className="settings-menu-item"
-          onClick={() => toggleForm("terminate")}
+          
         >
           Terminate
         </h3>
+        </Button>
       </div>
 
       {activeForm === "user" && <SettingsUserForm />}
