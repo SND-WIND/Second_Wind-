5c4c4a9fb086522f4a5d387e686d809e0370363f
BayzU <bayzedullah@gmail.com>
2023-06-29 04:30:24 -0400

    Added upload image

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,10 +1,38 @@
-import { useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
 import PostList from "./PostList";
+import { Widget } from "@uploadcare/react-widget";
+import { updateUserInfo } from "../adapters/user-adapter";
 
 export default function userProfile() {
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-  console.log(currentUser);
+  const [uploadedCoverImage, setUploadedCoverImage] = useState(
+    currentUser.cover_image
+  );
+  const [uploadedProfileImage, setUploadedProfileImage] = useState(
+    currentUser.profile_image
+  );
+
+  useEffect(() => {
+    const handleCoverChange = async () => {
+      const updatedCoverImage = await updateUserInfo({
+        cover_image: uploadedCoverImage,
+        id: currentUser.id,
+      });
+    };
+    handleCoverChange();
+  }, [uploadedCoverImage]);
+
+  useEffect(() => {
+    const handleProfileChange = async () => {
+      const updatedProfileImage = await updateUserInfo({
+        profile_image: uploadedProfileImage,
+        id: currentUser.id,
+      });
+    };
+    handleProfileChange();
+  }, [uploadedProfileImage]);
+
   return (
     <div className="user-profile-container">
       <div className="profile-cover-pic">
@@ -12,19 +40,32 @@ export default function userProfile() {
           className="cover-img"
           style={{ backgroundImage: `url(${currentUser.cover_image})` }}
         >
-          {" "}
+          <div className="upload-cover">
+            <Widget
+              publicKey="7363573380cc43836898"
+              onChange={(file) => setUploadedCoverImage(file && file.cdnUrl)}
+            />
+          </div>
           <div
             className="profile-img"
             style={{ backgroundImage: `url(${currentUser.profile_image})` }}
           ></div>
+          <div className="upload-profile">
+            <Widget
+              publicKey="7363573380cc43836898"
+              onChange={(file) => setUploadedProfileImage(file && file.cdnUrl)}
+            />
+          </div>
         </div>
       </div>
       <div className="profile-info">
-        <h4 className="profile-username">@{currentUser.username}</h4>
-        <h4 className="profile-name">{currentUser.fullName}</h4>
-        <div className="profile-about-me">
-          <h4>About Me</h4>
-          <p className="profile-bio">{currentUser.bio}</p>
+        <div>
+          <h4 className="profile-username">@{currentUser.username}</h4>
+          <h4 className="profile-name">{currentUser.fullName}</h4>
+          <div className="profile-about-me">
+            <h4>About Me</h4>
+            <p className="profile-bio">{currentUser.bio}</p>
+          </div>
         </div>
       </div>
       <PostList />
